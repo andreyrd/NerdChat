a589dc8ff9536319ff72ad5cb8acfdd6b66a5e4d
Dave Burkett <Dave.Burkett@cutterbuck.com>
2015-10-12 11:08:43 -0700

    added class RFC1459

---
+++ NerdChat/RFC1459.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdChat
+{
+    /// <summary>
+    /// https://tools.ietf.org/html/rfc1459
+    /// </summary>
+    class RFC1459
+    {
+        public static readonly IEnumerable<string> commands = new List<string>()
+            {
+                "ADMIN",
+                "AWAY",
+                "CONNECT",
+                "ERROR",
+                "INFO",
+                "INVITE",
+                "ISON",
+                "JOIN",
+                "KICK",
+                "KILL",
+                "LINKS",
+                "LIST",
+                "MODE",
+                "NAMES",
+                "NICK",
+                "NOTICE",
+                "OPER",
+                "PART",
+                "PASS",
+                "PING",
+                "PONG",
+                "PRIVMSG",
+                "QUIT",
+                "REHASH",
+                "RESTART",
+                "SERVER",
+                "SQUIT",
+                "STATS",
+                "SUMMON",
+                "TIME",
+                "TOPIC",
+                "TRACE",
+                "USER",
+                "USERHOST",
+                "USERS",
+                "VERSION",
+                "WALLOPS",
+                "WHO",
+                "WHOIS",
+                "WHOWAS"
+        };
+    }
+}
